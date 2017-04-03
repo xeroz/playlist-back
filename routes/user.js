7bddd747b1fb7edd0f1031da2de7e020b6a8252d
juan <chavezvasquezjuan@gmail.com>
2017-04-02 22:22:40 -0500

    routes

--- routes/user.js
+++ routes/user.js
@@ -4,9 +4,11 @@ var express = require('express');
 var UserController = require('../controllers/UserController');
 
 var api = express.Router();
+var md_auth = require('../middlewares/authenticated');
 
-api.get('/pruebas-ps', UserController.pruebas);
+api.get('/pruebas-ps', md_auth.ensureAuth, UserController.pruebas);
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.loginUser);
+api.put('/update-user/:id', md_auth.ensureAuth, UserController.updateUser);
 
 module.exports = api;
