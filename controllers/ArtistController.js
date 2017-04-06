82f347b064f8a74b75fa0eefeba0990d1e55737a
juan <chavezvasquezjuan@gmail.com>
2017-04-06 11:35:25 -0500

    artist controller

---
+++ controllers/ArtistController.js
@@ -0,0 +1,40 @@
+'use strict'
+
+var fs = require('fs');
+var path = require('path');
+
+var Artist = require('../models/artist');
+var Album = require('../models/album');
+var Song = require('../models/song');
+
+
+function getArtist(req, res){
+   res.status(200).send({ message: 'Método getArtist del controlador artist.js' });
+}
+
+function saveArtist(req, res){
+   var artist = new Artist();
+
+   var params = req.body;
+
+   artist.name        = params.name;
+   artist.description = params.description;
+   artist.image = 'null';
+
+   artist.save((err, artistStored) => {
+      if (err) {
+         res.status(500).send({ message: "Error al guardar el artista" });
+      } else {
+         if (!artistStored) {
+            res.status(404).send({ message: "El artista no ha sido guardado" });
+         } else {
+            res.status(200).send({ artist: artistStored });
+         }
+      }
+   });
+}
+
+module.exports = {
+   getArtist,
+   saveArtist
+}
