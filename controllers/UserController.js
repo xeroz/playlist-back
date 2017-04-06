7b68b5b1d916cd18cccbe30e11f12276966930d8
juan <chavezvasquezjuan@gmail.com>
2017-04-05 22:11:57 -0500

    add function uploadImage, getImageFile

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,7 @@
 'use strict'
 
+var fs = require('fs');
+var path = require('path');
 var bcrypt = require('bcrypt-nodejs');
 var User = require('../models/user');
 var jwt = require('../services/jwt');
@@ -97,9 +99,53 @@ function updateUser(req, res){
    });
 }
 
+function uploadImage(req, res){
+   var userId = req.params.id;
+   var file_name = 'No subio';
+
+   if (req.files) {
+      var file_path = req.files.image.path;
+      var file_split = file_path.split('\/');
+      var file_name = file_split[2];
+
+      var ext_split = file_name.split('\.');
+      var file_ext = ext_split[1];
+
+      if (file_ext == 'png' || file_ext == 'jpg' || file_ext == 'gif' ) {
+         User.findByIdAndUpdate(userId, {image: file_name}, (err, userUpdated) => {
+            if (!userUpdated) {
+               res.status(404).send({ message: 'No se ha podido actualizar el usuario' });
+            } else {
+               res.status(200).send({ user: userUpdated });
+            }
+         });
+      } else {
+         res.status(200).send({ message: 'Extension no es correcta' });
+      }
+      console.log(ext_split);
+   } else {
+      res.status(200).send({ message: 'No has subido ninguna imagen' });
+   }
+}
+
+function getImageFile(req, res){
+   var imageFile = req.params.imageFile;
+   var path_file = './uploads/users/' + imageFile;
+
+   fs.exists(path_file, function(exists){
+      if (exists) {
+         res.sendFile(path.resolve(path_file));
+      } else {
+         res.status(200).send({ message: 'No existe la imagen' });
+      }
+   });
+}
+
 module.exports = {
    pruebas,
    saveUser,
    loginUser,
-   updateUser
+   updateUser,
+   uploadImage,
+   getImageFile
 };
