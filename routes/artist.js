7928a2c698e93b563af8b16e11661f1a72d55a35
juan <chavezvasquezjuan@gmail.com>
2017-04-06 11:35:45 -0500

    routes artist

---
+++ routes/artist.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var express = require('express');
+var ArtistController = require('../controllers/ArtistController');
+var api = express.Router();
+var md_auth = require('../middlewares/authenticated');
+
+api.get('/artist', md_auth.ensureAuth, ArtistController.getArtist);
+api.post('/artist', md_auth.ensureAuth, ArtistController.saveArtist);
+
+module.exports = api;
