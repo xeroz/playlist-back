da84e3f5a04f0c87864a8a72671a59294458b20a
juan <chavezvasquezjuan@gmail.com>
2017-04-13 23:22:10 -0500

    album routes

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var app = express();
 //cargar rutas
 var user_routes   = require('./routes/user');
 var artist_routes = require('./routes/artist');
+var album_routes = require('./routes/album');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
@@ -17,5 +18,6 @@ app.use(bodyParser.json());
 //rutas base
 app.use('/api', user_routes);
 app.use('/api', artist_routes);
+app.use('/api', album_routes);
 
 module.exports = app;
