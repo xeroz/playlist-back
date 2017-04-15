0622c441e8df1a6229ea5a9d44010d15cfe3d29f
juan <chavezvasquezjuan@gmail.com>
2017-04-14 20:52:01 -0500

    app.js

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var app = express();
 //cargar rutas
 var user_routes   = require('./routes/user');
 var artist_routes = require('./routes/artist');
-var album_routes = require('./routes/album');
+var album_routes  = require('./routes/album');
+var song_routes   = require('./routes/song');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
@@ -19,5 +20,6 @@ app.use(bodyParser.json());
 app.use('/api', user_routes);
 app.use('/api', artist_routes);
 app.use('/api', album_routes);
+app.use('/api', song_routes);
 
 module.exports = app;
